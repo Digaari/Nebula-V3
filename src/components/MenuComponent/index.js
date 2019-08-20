791bb690816b6df42e551be88e2366ee7025cf86
Digaari <hrishabhdigaari@gmail.com>
2019-08-20 23:11:16 +0530

    first commit

--- src/components/MenuComponent/index.js
+++ src/components/MenuComponent/index.js
@@ -2,13 +2,17 @@ import AppBar from 'material-ui/AppBar';
 import FlatButton from 'material-ui/FlatButton';
 import PropTypes from 'prop-types';
 import React from 'react';
+import MenuAppBar from '../MenuComponent/appbar';
 
-const Menu = props => <AppBar
+const Menu = props => 
+<MenuAppBar/>
+
+{/* <AppBar
     title="App Title"
     onTitleClick={props.goHome}
     iconElementRight={<FlatButton label="Change Route"  />}
     onRightIconButtonClick={props.onClick}
-/>
+/> */}
 
 Menu.propTypes = {
  goHome: PropTypes.func.isRequired
